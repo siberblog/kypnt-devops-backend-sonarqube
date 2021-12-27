b922206734c2c76940287781fc7035a967858a77
Aytekin Yaliz <aytekin.yaliz@wipro.com>
2021-12-27 23:59:09 +0000

    initials

---
+++ src/main/java/com/kypnt/devopsbackend/KypntDevopsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kypnt.devopsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KypntDevopsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KypntDevopsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kypnt/devopsbackend/KypntDevopsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kypnt.devopsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KypntDevopsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
