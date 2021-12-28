fa157677585ff3dcd47a21c753ec10802f1cf443
Bekir Yalcin <70541124+siberblog@users.noreply.github.com>
2021-12-28 17:45:32 +0000

    Update App.java

--- src/main/java/com/kypnt/devopsbackend/App.java
+++ src/main/java/com/kypnt/devopsbackend/App.java
@@ -13,6 +13,13 @@ public class App
 
     private final String getMessage() {
         return message;
+        return message;
+        return message;
+        return message;
+        return message;
+        return message;
+        return message;
+        
     }
 
 }
