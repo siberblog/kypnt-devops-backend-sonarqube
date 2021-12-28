208818c4418ceba64d8a2a1734519a7c5bed5bdd
Bekir Yalcin <70541124+siberblog@users.noreply.github.com>
2021-12-28 10:27:51 +0000

    Update KypntDevopsBackendApplication.java

--- src/main/java/com/kypnt/devopsbackend/KypntDevopsBackendApplication.java
+++ src/main/java/com/kypnt/devopsbackend/KypntDevopsBackendApplication.java
@@ -11,3 +11,54 @@ public class KypntDevopsBackendApplication {
 	}
 
 }
+
+public class App
+{
+
+    private final String message = "Hello World from Java Maven Project!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
+
+public class App
+{
+
+    private final String message = "Hello World from Java Maven Project!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
+
+public class App
+{
+
+    private final String message = "Hello World from Java Maven Project!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
