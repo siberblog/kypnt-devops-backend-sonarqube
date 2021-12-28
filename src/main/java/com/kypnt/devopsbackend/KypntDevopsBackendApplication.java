035b5312ef6c8d0cb599fb493884752c6c6c5392
Bekir Yalcin <70541124+siberblog@users.noreply.github.com>
2021-12-28 10:47:33 +0000

    Update KypntDevopsBackendApplication.java

--- src/main/java/com/kypnt/devopsbackend/KypntDevopsBackendApplication.java
+++ src/main/java/com/kypnt/devopsbackend/KypntDevopsBackendApplication.java
@@ -11,18 +11,5 @@ public class KypntDevopsBackendApplication {
 	}
 }
 
-public class KypntDevopsBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(KypntDevopsBackendApplication.class, args);
-	}
-}
-
-public class KypntDevopsBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(KypntDevopsBackendApplication.class, args);
-	}
-}
 
 
